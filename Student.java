a9a6c2f6614ad32518e4cb42766c529660e356d4
Akhil04-E <akhiledulakanthi@gmail.com>
2025-08-06 21:05:44 +0530

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,34 @@
+package Task2;
+
+class Student {
+    private int id;
+    private String name;
+    private double marks;
+
+    public Student(int id, String name, double marks) {
+        this.id = id;
+        this.name = name;
+        this.marks = marks;
+    }
+
+    // Getters and Setters
+    public int getId() {
+        return id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setMarks(double marks) {
+        if(marks>=0 && marks<=100) {
+            this.marks = marks;
+        }else {
+            System.out.println("invalid marks");
+        }
+    }
+
+    public void display() {
+        System.out.println("ID: " + id + ", Name: " + name + ", Marks: " + marks);
+    }
+}
